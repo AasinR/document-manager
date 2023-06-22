0cf5ed924199701fc330499312258ffb83263f20
AasinR <bazsi2001018@gmail.com>
2023-06-22 23:03:20 +0200

    added spinner button component

---
+++ frontend/src/components/SpinnerButton.tsx
@@ -0,0 +1,47 @@
+import { ClipLoader } from "react-spinners";
+import React, { useState } from "react";
+
+function SpinnerButton({
+    children,
+    id,
+    className,
+    onClick,
+    spinnerColor,
+    spinnerSize,
+    speedMultiplier,
+}: SpinnerButtonType) {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const handleOnClick = (
+        event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => {
+        event.preventDefault();
+        if (!onClick) return;
+        (async () => {
+            setIsLoading(true);
+            await onClick();
+            setIsLoading(false);
+        })();
+    };
+
+    return (
+        <button
+            id={id}
+            className={className}
+            onClick={handleOnClick}
+            disabled={isLoading}
+        >
+            {isLoading ? (
+                <ClipLoader
+                    color={spinnerColor}
+                    size={spinnerSize}
+                    speedMultiplier={speedMultiplier}
+                />
+            ) : (
+                children
+            )}
+        </button>
+    );
+}
+
+export default SpinnerButton;
